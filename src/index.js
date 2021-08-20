d37ca07ffb4976663db6b5440d1c5a94be92ad46
ArielOnoriaga <arielonoriaga2015@gmail.com>
2021-08-20 19:38:44 -0300

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const filePaths = core.getInput('file');
+  const filePaths = core.getInput('files');
 
   console.log(filePaths);
 } catch (error) {
