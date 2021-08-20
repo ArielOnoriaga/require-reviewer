0e2a2638f17564b1c3f828ea110e63e0bd04a07f
ArielOnoriaga <arielonoriaga2015@gmail.com>
2021-08-20 19:17:06 -0300

    test: install githubcore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-(async () => {
-  console.log('this is a test');
-})();
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const filePaths = core.getInput('FILES');
+  console.log(filePaths);
+
+} catch (error) {
+  core.setFailed(error.message);
+}
