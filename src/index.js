056db44f88ff5be3152cab2279e5e0d4c5f55f68
ArielOnoriaga <arielonoriaga2015@gmail.com>
2021-08-20 19:36:44 -0300

    fix: fix typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const filePaths = core.getInput('file-paths');
+  const filePaths = core.getInput('file');
 
   console.log(filePaths);
 } catch (error) {
